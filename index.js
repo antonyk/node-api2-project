7399dfe33e8d15e25d1980953a90ef0bb286d4ba
Anthony K <antonyk@users.noreply.github.com>
2020-05-05 18:17:45 -0400

    added posts router

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express')
-// const postsRouter = require('./posts/posts-router')
+const express = require('express');
+const cors = require('cors');
+const postsRouter = require('./posts/posts-router');
 
 // Configure server
 const server = express();
@@ -7,11 +8,18 @@ const port = 5000;
 // Middleware
 // 1. Enable json
 server.use(express.json());
-// 2. Apply all routers
+// 2. Enable cors
+server.use(cors());
+// 3. Apply Base URL
+const api = express.Router();
+server.use('/api/v1', api);
+// 3. Apply all other routers
+api.use('/posts', postsRouter);
 
-
-
+api.get('/', (req, res) => {
+  res.status(200).send("API is up and running!")
+})
 
 server.listen(port, () => {
-  console.log("\n=== Server listening ===\n");
+  console.log(`\n=== Server listening on port ${port} ===\n`);
 })
---
+++ posts/posts-router.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const Posts = require('../data/db');
+
+
+
+module.exports = router;
