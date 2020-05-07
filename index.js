079d65dcbf01720bec7454c113c4eb153e3789a3
Anthony K <antonyk@users.noreply.github.com>
2020-05-07 13:25:17 -0400

    port assignment bug fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const postsRouter = require('./posts/posts-router');
 
 // Configure server
 const server = express();
-const port = process.env | 5000;
+const port = process.env.PORT || 5000;
 // Middleware
 // 1. Enable json
 server.use(express.json());
